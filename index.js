4fac7b4d445dcc1b55680d5041129727bfd37489
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-14 12:17:42 +0600

    git commit

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ document.getElementById("make-blue").addEventListener("click", function () {
 
 // how to change defult text or any thing
 
-function makemeChange() {
-    const changeME = document.getElementById("make-me-change")
-    changeME.innerText = "I heat this world"
-}
+// function makemeChange() {
+//     const changeME = document.getElementById("make-me-change")
+//     changeME.innerText = "I heat this world"
+// }
+//  same function another away
+//  document.getElementById("make-btn").addEventListener("click", function(){
+//     const changeIt = document.getElementById("make-me-change")
+//     changeIt.innerText = "I heat This world"
+//  })
+
+// i want whean i click in update the the title is changeIt
+
+// document.getElementById("update-btn").addEventListener("click", function(){
+//     const inputField = document.getElementById("input-field");
+//     const inputText = inputField.value;
+//     const p = document.getElementById("input-text-display")
+//     p.innerText = inputText
+//     inputField.value ="";
+// })
+
+
